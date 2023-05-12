99380da12e0fd68b4accd02a4845003374b80de5
Ioannis Skardasis <iskardasis@hotmail.com>
2023-05-12 21:04:41 +0300

    change in the mailer component,  name is readonly, email will be sent to the app admin

--- client/taxi-treasure/src/Components/Mailer.js
+++ client/taxi-treasure/src/Components/Mailer.js
@@ -1,8 +1,15 @@
 import emailjs from "emailjs-com";
+import jwt_decode from "jwt-decode";
+
+let token = null;
+token = localStorage.getItem("token");
+const decodedToken = jwt_decode(token);
+const username = decodedToken.username;
 
 const Mailer = () => {
   function sendEmail(e) {
     e.preventDefault();
+    console.log(username);
 
     emailjs
       .sendForm(
@@ -26,9 +33,9 @@ const Mailer = () => {
       <h1>Contact form</h1>
       <form onSubmit={sendEmail}>
         <label htmlFor=''>name</label>
-        <input type='text' name='name' />
-        <label htmlFor=''>email</label>
-        <input type='text' name='user_email' />
+        <input type='text' name='name' defaultValue={username} readOnly />
+        {/* <label htmlFor=''>email</label>
+        <input type='text' name='user_email' /> */}
         <label htmlFor=''>message</label>
         <textarea name='message' rows='4' />
         <input type='submit' value='submit' />
